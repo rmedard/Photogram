8b54d24fe851180dc16b7e463d034fdfa2184e79
rmedard <rmedard5@gmail.com>
2014-12-29 19:39:58 +0100

    Update sessions

---
+++ Photogram/src/be/kayiranga/controller/UpdateSessions.java
@@ -0,0 +1,33 @@
+package be.kayiranga.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import be.kayiranga.dao.FollowshipDao;
+import be.kayiranga.dao.UserDao;
+import be.kayiranga.daoImpl.FollowshipDaoImpl;
+import be.kayiranga.daoImpl.UserDaoImpl;
+import be.kayiranga.model.User;
+
+public class UpdateSessions {
+
+	private UserDao userDao;
+	private FollowshipDao followshipDao;
+
+	public UpdateSessions() {
+		super();
+		userDao = new UserDaoImpl();
+		followshipDao = new FollowshipDaoImpl();
+	}
+
+	public void update(List<HttpSession> sessions) {
+		for (HttpSession httpSession : sessions) {
+			if (httpSession.getAttribute("userId") != null) {
+				User user = userDao.findUserById(Integer.parseInt(httpSession
+						.getAttribute("userId").toString()));
+				followshipDao.sortFollowships(user, httpSession);
+			}
+		}
+	}
+}
